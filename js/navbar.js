de847b86838b86b55feccece8b88ec8b98b85aa2
Maurício L. Areias <95537796+mauricioareias@users.noreply.github.com>
2026-06-17 14:49:30 -0300

    js

---
+++ js/navbar.js
@@ -0,0 +1,90 @@
+const hamburgerMenu = document.getElementById('hamburger-menu')
+
+const navOffsetOverlay = document.getElementById('nav-offset-overlay')
+
+function openNavbar(element) {
+    element.classList.toggle('active');
+    navbar.classList.toggle('active');
+
+    if (navbar.classList.contains('active')) {
+        navOffsetOverlay.style.opacity = '0.5';
+        navOffsetOverlay.style.visibility = 'visible';
+    }
+
+    else {
+        navOffsetOverlay.style.opacity = '0';
+        navOffsetOverlay.style.visibility = 'hidden';
+    }
+}
+
+document.querySelectorAll('.navbar-elements a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        navOffsetOverlay.style.opacity = '0';
+        navOffsetOverlay.style.visibility = 'hidden';
+
+        hamburgerMenu.classList.toggle('active');
+        navbar.classList.remove('active');
+
+        const targetId = this.getAttribute('href');
+        const target = document.querySelector(targetId);
+
+        if (target) {
+            gsap.to(window, {
+                duration: 1.2,
+                scrollTo: {
+                    y: target,
+                    offsetY: 0 // ajuste se tiver header fixo
+                },
+                ease: 'power2.out'
+            });
+
+            e.preventDefault();
+        }
+    });
+});
+
+document.querySelector('.header-icon a').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    navOffsetOverlay.style.opacity = '0';
+    navOffsetOverlay.style.visibility = 'hidden';
+
+    hamburgerMenu.classList.remove('active');
+    navbar.classList.remove('active');
+
+    const targetId = this.getAttribute('href'); // ex: "#sec2"
+    const target = document.querySelector(targetId);
+
+    if (target) {
+        gsap.to(window, {
+            duration: 1.2,
+            scrollTo: {
+                y: target,
+                offsetY: 0 // ajuste se tiver header fixo
+            },
+            ease: 'power2.out'
+        });
+    }
+
+    if (target) {
+        gsap.to(window, {
+            duration: 1.2,
+            scrollTo: {
+                y: target,
+                offsetY: 0 // ajuste se tiver header fixo
+            },
+            ease: 'power2.out'
+        });
+    }
+});
+
+document.addEventListener('click', function (event) {
+    if (!navbar.contains(event.target) && !hamburgerMenu.contains(event.target) && navbar.classList.contains('active')) {
+        navOffsetOverlay.style.opacity = '0';
+        navOffsetOverlay.style.visibility = 'hidden';
+
+        hamburgerMenu.classList.remove('active');
+        navbar.classList.remove('active');
+
+    }
+});
